13733862663bc5ac6736c52e4724481ad2eaca32
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-03 14:52:16 +0200

    end

--- js/style.js
+++ js/style.js
@@ -4,3 +4,30 @@
 //partite da un semplice log in console,
 //ma poi scrivetelo in pagina
 //e se volete aggiungete un pò di CSS per rednerlo più carino
+
+// chiedo al utente il suo Nome 
+let userName = prompt("iserisci il tuo Nome");
+
+console.log(userName);
+// chiedo al utente il suo Cognome
+let userLastName = prompt("inserisci il tuo cognome");
+
+console.log(userLastName);
+// chiedi al utente il colore preferito
+let userColor = prompt ("inserisci il tuo colore preferito");
+
+console.log(userColor);
+// risultato della somma delle diverse variabili
+let result =`${userName}${userLastName}${userColor}23`;
+
+console.log(result);
+//stampo il risultato in html
+document.getElementById("pass").innerHTML = result;
+// cambio il colore del risultato
+document.getElementById("pass").style.color = "red";
+// centro il testo
+document.getElementById("pass").style.textAlign = "center";
+// cambio il formato
+document.getElementById("pass").style.fontSize = "xxx-large";
+// metto il bordo
+document.getElementById("pass").style.border = "solid";
