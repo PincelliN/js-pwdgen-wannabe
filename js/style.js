64855e2dadc8af96c38b550ff970c2cb3b3dcf9d
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-03 16:34:57 +0200

    add color style with js

--- js/style.js
+++ js/style.js
@@ -5,7 +5,7 @@
 //ma poi scrivetelo in pagina
 //e se volete aggiungete un pò di CSS per rednerlo più carino
 
-// chiedo al utente il suo Nome 
+// chiedo al utente il suo Nome
 let userName = prompt("iserisci il tuo Nome");
 
 console.log(userName);
@@ -14,20 +14,16 @@ let userLastName = prompt("inserisci il tuo cognome");
 
 console.log(userLastName);
 // chiedi al utente il colore preferito
-let userColor = prompt ("inserisci il tuo colore preferito");
+let userColor = prompt("inserisci il tuo colore preferito in inglese");
 
 console.log(userColor);
 // risultato della somma delle diverse variabili
-let result =`${userName}${userLastName}${userColor}23`;
+let result = `${userName}${userLastName}${userColor}23`;
 
 console.log(result);
 //stampo il risultato in html
 document.getElementById("pass").innerHTML = result;
 // cambio il colore del risultato
-document.getElementById("pass").style.color = "red";
-// centro il testo
-document.getElementById("pass").style.textAlign = "center";
-// cambio il formato
-document.getElementById("pass").style.fontSize = "xxx-large";
-// metto il bordo
-document.getElementById("pass").style.border = "solid";
+document.getElementById("pass").style.color = userColor;
+
+
