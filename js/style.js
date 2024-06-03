c8156962b6379a8539f595682f04984cf3cddf97
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-03 14:07:57 +0200

    start

---
+++ js/style.js
@@ -0,0 +1,6 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+//Infine scrivi sulla pagina nomecognomecolorepreferito23
+//Output:
+//partite da un semplice log in console,
+//ma poi scrivetelo in pagina
+//e se volete aggiungete un pò di CSS per rednerlo più carino
